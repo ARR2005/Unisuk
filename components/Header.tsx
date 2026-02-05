7e804954a74f761c0ecdfdc95128134da58d45ac
ARR2005 <Aldwinrich90@gmail.com>
2026-02-05 13:44:31 +0800

    fix safe area view added app builder

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import { Image, View, useColorScheme } from 'react-native';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
 const Header: React.FC = () => {
   const colorScheme = useColorScheme();
   const isDark = colorScheme === 'dark';
   const bgColor = isDark ? 'bg-gray-900' : 'bg-white';
   const borderColor = isDark ? 'border-gray-800' : 'border-gray-100';
+  const insets = useSafeAreaInsets();
 
   return (
-    <View className={`h-16 justify-center items-start px-5 ${bgColor} border-b ${borderColor}`}>
-      <Image source={require('../assets/icons/Logo.png')} className='w-32 h-12' resizeMode='contain' />
+    <View style={{ paddingTop: insets.top, backgroundColor: isDark ? '#111827' : '#fff' }}>
+      <View className={`h-20 justify-center items-center px-5 border-b ${borderColor}`}>
+        <Image source={require('../assets/icons/Logo.png')} className='w-48 h-48' resizeMode='contain' />
+      </View>
     </View>
   );
 };
